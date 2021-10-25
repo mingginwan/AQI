007988b932312c9d1dd943c6a041bd69999f4cf8
mingginwan <91401222+mingginwan@users.noreply.github.com>
2021-10-25 17:49:31 +0800

    Update AQI-US.js

--- AQI-US.js
+++ AQI-US.js
@@ -14,8 +14,8 @@ const aqicnToken = 'd8583308daa94866469bc92f567d75420807391c'
 */
 
 const AirQualityStandard = {
-	CN: 'HJ6332012.2111',
-	US: 'EPA_NowCast.2111'
+	CN: 'HJ6332012.2113',
+	US: 'EPA_NowCast.2113'
 }
 
 const AirQualityLevel = {
